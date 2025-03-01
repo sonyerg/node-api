1fb9e9ade4378fd9442ea367e2d04527bc7f6a05
sonyerg <ericksonrguinto@gmail.com>
2025-03-01 21:45:43 +1300

    added options header

--- app.js
+++ app.js
@@ -44,9 +44,13 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
     "Access-Control-Allow-Methods",
-    "GET, POST, PUT, PATCH, DELETE"
+    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
   );
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
 
   next();
 });
